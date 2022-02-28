a9bfc6c8c55705b5bfa6f29f3a0a9f8e4c0c6eef
HamadBurahmah <43260627+HamadBurahmah@users.noreply.github.com>
2022-02-28 14:36:25 -0800

    main.cpp
    line 60ish deleted

--- main.cpp
+++ main.cpp
@@ -60,8 +60,6 @@ std::string get_hint(std::string match, std::string word){
     return word;
 }
 
-=======
-
 // capitalizes a word (to UPPER CASE)
 void capitalize(std::string & word){
     for(int i=0; i<word.length(); i++){
